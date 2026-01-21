c4dac267d1aa6bcff76c7b029dcbeadeaf98d875
emilsandberg12-lab <emilsandberg12@gmail.com>
2026-01-21 20:02:44 +0100

    ndex.js

--- index.js
+++ index.js
@@ -3,34 +3,21 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Shopify sender ofte HEAD requests først
-app.head("/api/sitemap", (req, res) => {
-  res.status(200).end();
-});
-
-// Sitemap endpoint (GET)
-app.get("/api/sitemap", (req, res) => {
+// Shopify App Proxy – svar altid OK
+app.use((req, res) => {
+  res.status(200);
   res.setHeader("Content-Type", "application/xml");
-  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
+
+  res.send(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://decisionlab.myshopify.com/</loc>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
-  </url>
-</urlset>`);
-});
-
-// Root (kun til test)
-app.get("/", (req, res) => {
-  res.status(200).send("DecisionLab Sitemap is running");
-});
-
-// Catch-all (vigtigt for Shopify)
-app.use((req, res) => {
-  res.status(200).end();
+  </urlset>`);
 });
 
 app.listen(PORT, () => {
-  console.log(`Sitemap app running on port ${PORT}`);
+  console.log("DecisionLab Sitemap app running");
 });
+
